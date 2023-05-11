a65ae303a3aa8a5a2b88d34d801679b849ebdea1
Alan9435 <s990611@kimo.com>
2023-05-11 20:19:43 +0800

    新增加解密單元測試

--- tutorial/src/main/java/com/example/tutorial/Fragment/RsaFragment.kt
+++ tutorial/src/main/java/com/example/tutorial/Fragment/RsaFragment.kt
@@ -22,24 +22,30 @@ class RsaFragment: BaseFragment<FragmentRsaBinding>() {
         val publicKey = keyPair.public as RSAPublicKey  //產生公鑰
         val privateKey = keyPair.private as RSAPrivateKey   //產生私鑰
 
-        /**
-         * Cipher物件是Java加密API提供的一個類別，用於執行加密或解密操作。
-         * 在這裡，我們使用Cipher.getInstance("RSA/ECB/PKCS1Padding")方法獲取一個執行RSA算法的Cipher物件，其中：
-         * "RSA"表示使用RSA算法；
-         * "ECB"表示使用電子密碼本模式（Electronic Codebook mode）；
-         * "PKCS1Padding"表示使用RSA加密填充模式之一，用於確保加密的數據能夠適當地進行填充，以防止攻擊者從加密的數據中推斷出任何有用的信息。*/
+        decryptCode(encryptCode("test123", publicKey = publicKey),privateKey)
+    }
+
+    /**
+     * Cipher物件是Java加密API提供的一個類別，用於執行加密或解密操作。
+     * 在這裡，我們使用Cipher.getInstance("RSA/ECB/PKCS1Padding")方法獲取一個執行RSA算法的Cipher物件，其中：
+     * "RSA"表示使用RSA算法；
+     * "ECB"表示使用電子密碼本模式（Electronic Codebook mode）；
+     * "PKCS1Padding"表示使用RSA加密填充模式之一，用於確保加密的數據能夠適當地進行填充，以防止攻擊者從加密的數據中推斷出任何有用的信息。*/
+    private fun encryptCode(code: String,publicKey: RSAPublicKey): ByteArray? {
         //指定要使用RSA/ECB/PKCS1Padding加密解密方式
         val cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding")
         cipher.init(Cipher.ENCRYPT_MODE, publicKey)     //初始化加密
-        val encrypted = cipher.doFinal("test123".toByteArray(Charsets.UTF_8)) //使用RSA公鑰加密後得到結果
+        val encrypted = cipher.doFinal(code.toByteArray(Charsets.UTF_8)) //使用RSA公鑰加密後得到結果
         Log.d("***********", "加密後: ${encrypted}")
+        return encrypted
+    }
 
+    private fun decryptCode(code: ByteArray?,privateKey: RSAPrivateKey): String {
+        val cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding")
         cipher.init(Cipher.DECRYPT_MODE, privateKey)  //初始化解密
-        val decrypted = cipher.doFinal(encrypted)     //使用私鑰解密
+        val decrypted = cipher.doFinal(code)     //使用私鑰解密
         val decryptedText = String(decrypted, Charsets.UTF_8) //獲得解密
-
-        //使用單元測試來驗證加解密 藍芽 網路影片播放
-
-        Log.d("***********", "dec: ${decryptedText}")
+        Log.d("***********", "解密後: ${decryptedText}")
+        return decryptedText
     }
 }
---
+++ tutorial/src/test/java/com/example/tutorial/Fragment/RsaFragmentTest.kt
@@ -0,0 +1,37 @@
+package com.example.tutorial.Fragment
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import java.security.KeyPairGenerator
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import javax.crypto.Cipher
+
+@RunWith(JUnit4::class)
+class RsaFragmentTest {
+
+    @Test
+    fun verifyCode(){
+        val resultText = "test1234"
+        val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+        keyPairGenerator.initialize(2048)
+        val keyPair = keyPairGenerator.generateKeyPair()
+        val publicKey = keyPair.public as RSAPublicKey
+        val privateKey = keyPair.private as RSAPrivateKey
+
+        val cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding")
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey)
+        val encrypted = cipher.doFinal(resultText.toByteArray(Charsets.UTF_8))
+
+        cipher.init(Cipher.DECRYPT_MODE, privateKey)
+        val decrypted = cipher.doFinal(encrypted)
+        val decryptedText = String(decrypted, Charsets.UTF_8)
+
+        assertEquals(resultText,decryptedText) //使用斷言判斷加密前後字串相等
+    }
+}
