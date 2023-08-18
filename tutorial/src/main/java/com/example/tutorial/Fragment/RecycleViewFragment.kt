807b30f30a2a1fea79f3f27294054567612f2288
Alan <alan.lee@gomaji.com>
2023-08-18 09:59:25 +0800

    新增ListView與RecyclerView 效能對比

--- tutorial/src/main/java/com/example/tutorial/Fragment/RecycleViewFragment.kt
+++ tutorial/src/main/java/com/example/tutorial/Fragment/RecycleViewFragment.kt
@@ -1,22 +1,30 @@
 package com.example.tutorial.Fragment
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.common.Base.BaseFragment
 import com.example.common.Base.InterfaceCallback
 import com.example.tutorial.Fragment.Adapter.ItemData
 import com.example.tutorial.Fragment.Adapter.RecycleViewAdapter
 import com.example.tutorial.databinding.FragmentRecycleviewBinding
+import com.example.tutorial.databinding.ItemViewBinding
 
-class RecycleViewFragment: BaseFragment<FragmentRecycleviewBinding>(),InterfaceCallback {
-    lateinit var itemList: List<ItemData>
+/** 開Profiler觀察ListView跟RV的效能差別
+ * 可以發現當你使用ListView時上下滑動記憶體並不會被回收後重用
+ * 而RV由於 viewPool與viewHolder機制 使itemView可以不用一直被銷毀與創建 達到重用目的 */
+class RecycleViewFragment : BaseFragment<FragmentRecycleviewBinding>(), InterfaceCallback {
+    private var itemList: MutableList<ItemData> = mutableListOf()
     override fun onViewInit() {
-        itemList = listOf(
-            ItemData("t1","b1"),
-            ItemData("t2","b2"),
-            ItemData("t3","b3"),
-            ItemData("t4","b4"))
+        for (i in 0..100) {
+            itemList.add(ItemData("t$i", "b$i"))
+        }
+
         binding.rvMyItems.apply {
-            adapter = RecycleViewAdapter(itemList).apply{
+            adapter = RecycleViewAdapter(itemList).apply {
                 setListen(this@RecycleViewFragment)
                 /**
                  * this@RecycleViewFragment 表示當前 RecycleViewFragment 的實例，
@@ -28,9 +36,33 @@ class RecycleViewFragment: BaseFragment<FragmentRecycleviewBinding>(),InterfaceC
             layoutManager = LinearLayoutManager(context) //設定layout顯示方式
         }
 
+//        binding.lvMyItems.apply {
+//            adapter = ListViewAdapter(context,itemList)
+//        }
     }
 
-    override fun getItem(itemText: String){
+    override fun getItem(itemText: String) {
         binding.tvTitle.text = itemText
+    }
+}
+
+class ListViewAdapter(var context: Context, var data: List<ItemData>) : BaseAdapter() {
+    override fun getCount() = data.size
+
+    override fun getItem(position: Int): ItemData {
+        return data[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = ItemViewBinding.inflate(LayoutInflater.from(context))
+        view.tvItem.text = getItem(position).itemText
+        view.btnItem.apply {
+            text = getItem(position).itemButtonText
+        }
+        return view.root
     }
 }
