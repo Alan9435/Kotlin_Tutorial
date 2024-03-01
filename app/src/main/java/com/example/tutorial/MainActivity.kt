1b87b74e01c70aafaf5e6f02deb88bd8176332e6
Alan <alan.lee@gomaji.com>
2024-03-01 14:53:44 +0800

    調整master進入點

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -11,9 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //todo compose套件 voyager
-        //跳頁至compose畫面
-        startActivity(Intent(this, ProjectTestActivity::class.java))
     }
 }
