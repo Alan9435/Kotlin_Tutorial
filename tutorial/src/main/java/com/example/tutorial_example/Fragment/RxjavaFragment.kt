243b87f3e959d13c9cb27b5c97635fbb65bf4d5c
Alan <alan.lee@gomaji.com>
2024-03-05 16:58:35 +0800

    新增Rxjava case

--- tutorial/src/main/java/com/example/tutorial_example/Fragment/RxjavaFragment.kt
+++ tutorial/src/main/java/com/example/tutorial_example/Fragment/RxjavaFragment.kt
@@ -7,8 +7,10 @@ import com.example.common.Base.Util.RxSubscriber
 import com.example.tutorial_example.databinding.FragmentRxjavaBinding
 import io.reactivex.Flowable
 import io.reactivex.Observable
+import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
@@ -22,7 +24,7 @@ import java.util.concurrent.TimeUnit
  * Schedulers.newThread() 創建新執行續 適合短時間操作 ex:數據計算
  * Schedulers.computation() 計算執行續 適合耗時的計算操作 ex:圖像處理、數學計算
  * AndroidSchedulers.mainThread() 主執行續 android只能在主執行續更新UI */
-class RxjavaFragment: BaseFragment<FragmentRxjavaBinding>() {
+class RxjavaFragment : BaseFragment<FragmentRxjavaBinding>() {
     private val compositeDisposable = CompositeDisposable() //存放disposable的容器 利於註銷
 
     override fun onViewInit() {
@@ -32,24 +34,82 @@ class RxjavaFragment: BaseFragment<FragmentRxjavaBinding>() {
             testMap()
             fetchData()
             getInfo()
+            testZip()
         }
     }
 
-    private fun testFilter(){
-        val disposable = Observable.range(1,10)
+
+    /** zip 等待2個API都呼叫完成 */
+    private fun testZip() {
+        var i = 0
+        val obA = Observable.create<String> { e ->
+            Thread.sleep(5000)
+
+            //假設撥打3次才成功
+            if (i <= 2) {
+                e.onError(Throwable("a error"))
+            } else {
+                e.onNext("obA done")
+            }
+            i++
+            e.onComplete()
+        }.subscribeOn(Schedulers.io())
+
+        val obB = Observable.create<String> { e ->
+            Thread.sleep(3000)
+            e.onNext("obB done")
+            e.onComplete()
+        }.subscribeOn(Schedulers.io())
+
+        Observable.zip(
+            obA,
+            obB
+        ) { a, b ->
+            Log.d("*********", "test")
+            "testZip: done ${a + b}"
+        }
+//            .retry(3)  //retry時不會進到onError
+            .subscribe(getTheObserver())
+
+    }
+
+    private fun getTheObserver(): Observer<String> {
+        return object : Observer<String> {
+            override fun onNext(t: String) {
+                //2個API都成功的後續動作
+                Log.d("*********", t)
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                Log.d("*********", "onSubscribe")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d("*********", "onError : ${e.message}")
+            }
+
+            override fun onComplete() {
+                Log.d("*********", "onComplete")
+            }
+        }
+    }
+
+
+    private fun testFilter() {
+        val disposable = Observable.range(1, 10)
             .subscribeOn(Schedulers.io())   //切換至Io線程 subscribeOn切換執行緒並不會立即觸發執行緒切換，而是在事件發生時才進行切換
             .filter { it % 2 == 1 } //過濾條件 與kotlin filter用法一樣
             .observeOn(Schedulers.newThread())
-            .delay(500,TimeUnit.MICROSECONDS)
-            .subscribe{ result ->
+            .delay(500, TimeUnit.MICROSECONDS)
+            .subscribe { result ->
                 Log.d("****", "testFilter: $result")
             }
         compositeDisposable.add(disposable)
     }
 
-    private fun testMap(){
+    private fun testMap() {
         // 訂閱 Observable
-        val disposable = Observable.just("hi","word")
+        val disposable = Observable.just("hi", "word")
             .subscribeOn(Schedulers.io()) // 在 IO 執行緒中執行
             .observeOn(Schedulers.newThread()) // 在新的執行緒中觀察
             .map { it + "test" } // 對資料進行操作 這裡把字串都加入test結尾
@@ -61,7 +121,7 @@ class RxjavaFragment: BaseFragment<FragmentRxjavaBinding>() {
     }
 
     private fun fetchData() {
-        val disposable = Observable.just("Hello Reactive World" )//模擬API回傳
+        val disposable = Observable.just("Hello Reactive World")//模擬API回傳
             .subscribeOn(Schedulers.io())   //切換至Io線程
             .observeOn(AndroidSchedulers.mainThread()) //切換至主線程
             .subscribe(     //接收數據回傳 3個function callback
@@ -71,7 +131,7 @@ class RxjavaFragment: BaseFragment<FragmentRxjavaBinding>() {
                 { errorMsg ->
                     binding.tvTest.text = errorMsg.toString()
                 }, //錯誤
-                { Log.d("***********", ": finish")  } //完成
+                { Log.d("***********", ": finish") } //完成
             )
         compositeDisposable.add(disposable)  //disposable添加至CompositeDisposable 利於取消訂閱
     }
@@ -94,7 +154,7 @@ class RxjavaFragment: BaseFragment<FragmentRxjavaBinding>() {
             //getNewStoreInfo(storeId, groupId)的結果
             .subscribeWith(object : RxSubscriber<Any>() {
                 override fun _onNext(t: Any) {
-                    if(t is TestData){
+                    if (t is TestData) {
                         Log.d("******", "_onNext: 成功動作 ${t.title}")
                     }
                 }
